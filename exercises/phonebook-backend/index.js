36e7e3d20a80cfee1d01bf358c0166140483fc40
Skyler <goodsky6@gmail.com>
2022-01-12 21:23:55 -0800

    Exercises 3a: phonebook-backend

---
+++ exercises/phonebook-backend/index.js
@@ -0,0 +1,104 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+let people = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+];
+
+const MAX_ID = 1000000000;
+const generateId = () => {
+    return Math.floor(Math.random() * MAX_ID);
+};
+
+app.get('/info', (request, response) => {
+    const phonebookInfo = `Phonebook has info for ${people.length} people`;
+    const timestamp = new Date().toString();
+    response.send(`<p>${phonebookInfo}</p><p>${timestamp}</p>`);
+});
+
+app.get('/api/persons', (request, response) => {
+    console.log('Fetched persons', people.length);
+    response.json(people);
+});
+
+app.post('/api/persons', (request, response) => {
+    const person = request.body;
+    if (!person || !person.name || !person.number) {
+        console.log('Invalid data!');
+        return response.status(400).json({ error: 'Person must have "name" and "number".'});
+    }
+
+    if (people.find(p => p.name === person.name)) {
+        console.error('Phone book already contains person', person.name);
+        return response.status(409).json({ error: 'Person with name already exists.' });
+    }
+
+    const newPerson = {
+        id: generateId(),
+        name: person.name,
+        number: person.number,
+    };
+
+    if (people.find(p => p.id === newPerson.id)) {
+        console.error('Wow! We already have a person with id', p.id);
+        return response.status(500).json({ error: 'You just lost the lottery. Try calling again.' });
+    }
+
+    console.log('Adding person', newPerson);
+    people.push(newPerson);
+    response.status(201).json(newPerson);
+});
+
+app.get('/api/persons/:id', (request, response) => {
+    const rawId = request.params.id;
+    const id = Number(request.params.id);
+
+    const person = people.find(p => p.id === id);
+
+    if (person) {
+        console.log('Reading person', person.name);
+        response.json(person);
+    }
+    else {
+        console.log('Person does not exist', rawId);
+        response.sendStatus(404);
+    }
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+    const rawId = request.params.id;
+    const id = Number(request.params.id);
+
+    const person = people.find(p => p.id === id);
+    if (person) {
+        console.log('Deleting person', person.name);
+        people = people.filter(p => p.id !== id);
+    }
+    
+    response.sendStatus(204);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
